6be64b2c846a8901bc932db9b57351d7eaf16928
Haoyi Wu <wuhy1@shanghaitech.edu.cn>
2019-06-17 20:37:21 +0800

    style check of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -6,10 +6,10 @@ public class LeapYear {
      * Judge if a year is a leap year.
      * @param year
      */
-    public static boolean isLeapYear(int year){
-        if (year % 400 == 0){
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
             return true;
-        } else if (year % 4 == 0 && year % 100 != 0){
+        } else if (year % 4 == 0 && year % 100 != 0) {
             return true;
         }
         return false;
