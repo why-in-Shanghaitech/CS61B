a1e1efb7f60485d93ccc981eaa4c7c13c4e8bbcb
Haoyi Wu <wuhy1@shanghaitech.edu.cn>
2019-06-29 11:53:29 +0800

    finished hw1

--- hw1/synthesizer/TestArrayRingBuffer.java
+++ hw1/synthesizer/TestArrayRingBuffer.java
@@ -9,7 +9,27 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        int answer = arb.dequeue();
+        assertEquals(1, answer);
+        assertEquals(1, arb.fillCount());
+    }
+
+    @Test
+    public void testIterator() {
+        ArrayRingBuffer<Integer> arb = new ArrayRingBuffer<>(10);
+        arb.enqueue(1);
+        arb.enqueue(2);
+        arb.enqueue(3);
+        arb.enqueue(4);
+        int tar = 1;
+        for (int t: arb) {
+            assertEquals(tar, t);
+            tar++;
+        }
+        assertEquals(5, tar);
     }
 
     /** Calls tests for ArrayRingBuffer. */
